3e1b6a250d4ceb2f5d8954c637164fe701cd42b3
Jason Kadman <jkadman231@gmail.com>
2022-11-21 04:45:15 +0000

    minor cleanup before submission

--- play.js
+++ play.js
@@ -2,11 +2,9 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 const { IP, PORT } = require("./constants");
 
-// console.log(IP)
 let connection;
 
 console.log("Connecting ...");
-// connect();
 
 setupInput(connect(connection));
 
